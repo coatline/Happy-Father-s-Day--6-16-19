0cd803fd84ca83b43f2d7d0ad7019facb5c1b7d9
Bryler C <bryler.castle@gmail.com>
2022-08-04 15:41:34 -0400

    Initial Commit

---
+++ Project/Assets/Fireworks.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Fireworks : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    ParticleSystem ps;
+    bool emit;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        text.gameObject.SetActive(true);
+        ps.startColor = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !emit)
+        {
+            emit = true;
+        }
+
+        if (emit)
+        {
+            ps.Emit(200);
+            emit = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            text.text = "I love you!";
+        }
+    }
+}
